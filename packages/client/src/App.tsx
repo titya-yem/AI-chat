6a43fd521ac14d31acd4b1918d23dffe6ddc12c5
Titya Yem <thitya.yem.photo@gmail.com>
2025-11-04 05:56:20 -0500

    Setup: Shadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   const [message, setMessage] = useState("");
@@ -9,7 +10,12 @@ const App = () => {
       .then((data) => setMessage(data.message));
   }, []);
 
-  return <p>{message}</p>;
+  return (
+    <div className="pl-4">
+      <p className="font-bold text-3xl">{message}</p>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
