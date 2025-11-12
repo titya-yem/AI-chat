ada91041aba915ae9e3b77acd08bc03d7a623f51
Titya Yem <thitya.yem.photo@gmail.com>
2025-11-12 16:28:22 -0500

    Finished everything and ready for deployment

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
 import StarRating from './StarRating';
+import Skeleton from 'react-loading-skeleton';
+import { useQuery } from '@tanstack/react-query';
+import { Button } from '../ui/button';
+import { HiSparkles } from 'react-icons/hi2';
 
 type Props = {
   productId: number;
@@ -20,29 +23,65 @@ type GetReviewsResponse = {
 };
 
 const ReviewList = ({ productId }: Props) => {
-  const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+  const {
+    data: reviewData,
+    error,
+    isLoading,
+  } = useQuery<GetReviewsResponse>({
+    queryKey: ['reviews', productId],
+    queryFn: async () => fetchReviews(),
+  });
+
   const fetchReviews = async () => {
     const { data } = await axios.get<GetReviewsResponse>(
       `/api/products/${productId}/reviews`
     );
-    setReviewData(data);
+    return data;
   };
-  useEffect(() => {
-    fetchReviews();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-5">
+        {[1, 2, 3, 4, 5].map((i) => (
+          <div key={i}>
+            <Skeleton width={150} />
+            <Skeleton width={100} />
+            <Skeleton count={2} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="text-red-500">Could fetch reviews. Try again</p>;
+  }
+
+  if (!reviewData?.reviews.length) return null;
 
   return (
-    <div className="fkex flex-col gap-5">
-      {reviewData?.reviews.map((review) => (
-        <div key={review.id}>
-          <div className="font-semibold">{review.author}</div>
-          <div>
-            <StarRating value={review.rating} />
+    <div>
+      <div className="mb-5">
+        {reviewData?.summary ? (
+          <p>{reviewData.summary}</p>
+        ) : (
+          <Button>
+            <HiSparkles />
+            Summarizes
+          </Button>
+        )}
+      </div>
+      <div className="fkex flex-col gap-5">
+        {reviewData?.reviews.map((review) => (
+          <div key={review.id}>
+            <div className="font-semibold">{review.author}</div>
+            <div>
+              <StarRating value={review.rating} />
+            </div>
+            <div className="py-2">{review.content}</div>
           </div>
-          <div className="py-2">{review.content}</div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
